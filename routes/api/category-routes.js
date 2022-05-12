6b2052d185534c69f1a227b0dfa0973869bdfc3f
Gart <gmhobson@gmail.com>
2022-05-12 18:03:23 -0500

    more routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -67,10 +67,45 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({message: 'No Category with this ID'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({message: 'No Category with this ID'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
